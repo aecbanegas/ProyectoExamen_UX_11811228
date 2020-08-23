4d9948d19c4f2b8e682e02709d0d946c05df6664
Andrés Cruz <andrescruz@Andress-MacBook-Pro.local>
2020-08-23 14:25:29 -0600

    Reparacion de problemas e implementacion de localstorage - Se arreglo el problema de wrapping de textos - Se agrego funcionalidad de localstorage - Se integro search(aun no probado)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Text} from 'react';
+import React, { Fragment, Text } from 'react';
 import './App.css';
 
 
@@ -6,40 +6,50 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      act: 0,
-      index: '',
       datas: [],
+      globaldatas: [],
     }
   }
 
   componentDidMount() {
     this.refs.note.focus();
+    const json = localStorage.getItem('datas');
+    const datas = JSON.parse(json) || [];
+    const json2 = localStorage.getItem('globaldatas');
+    const globaldatas = JSON.parse(json2) || [];
+    this.setState({ datas, globaldatas });
   }
 
   fSubmit = (e) => {
-    const months = ["JAN", "FEB", "MAR","APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+    const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
     e.preventDefault();
     console.log('try');
 
     let datas = this.state.datas;
     let note = this.refs.note.value;
     let current_datetime = new Date()
-    let formatted_date = current_datetime.getDate() + "/" + months[current_datetime.getMonth()] + "/" + current_datetime.getFullYear() +" "+current_datetime.getHours()+":"+current_datetime.getMinutes()+":"+current_datetime.getSeconds()
+    let formatted_date = current_datetime.getDate() + "/" + months[current_datetime.getMonth()] + "/" + current_datetime.getFullYear() + " " + current_datetime.getHours() + ":" + current_datetime.getMinutes() + ":" + current_datetime.getSeconds()
     console.log(formatted_date)
     let date = formatted_date;
-
-
+    let tags_origen = this.refs.tags.value;
+    console.log(String(tags_origen));
+    let tags = String(tags_origen).split(',');
     let data = {
-      note, date,
+      note, date, tags
+    }
+    let globaldata = {
+      note, date, tags
     }
     datas.push(data);
-
+    let globaldatas = this.state.globaldatas;
+    globaldatas.push(globaldata)
 
     this.setState({
       datas: datas,
-      act: 0
+      globaldatas: globaldatas,
     });
-
+    localStorage.setItem('datas', JSON.stringify(datas));
+    localStorage.setItem('globaldatas', JSON.stringify(globaldatas));
     this.refs.myForm.reset();
     this.refs.note.focus();
   }
@@ -54,6 +64,28 @@ class App extends React.Component {
     this.refs.myForm.reset();
     this.refs.note.focus();
   }
+
+  fSearch = (e) => {
+    let searchWord = String(this.refs.wordsearch.value);
+    console.log("La palabra es " + searchWord);
+    let datas = this.state.datas;
+    let globaldatas = this.state.globaldatas;
+    if (searchWord = '') {
+      datas = globaldatas;
+    } else {
+      let arraydata = [];
+      for (let index = 0; index < globaldatas.length; index++) {
+        console.log(typeof globaldatas[index].tags)
+        console.log(globaldatas[index].tags)
+        if (globaldatas[index].tags.includes(searchWord)) {
+          arraydata.push(globaldatas[index]);
+          console.log("entra al if: " + globaldatas[index].tags)
+        }
+      }
+      datas = arraydata;
+    }
+  }
+
   render() {
     let datas = this.state.datas;
     return (
@@ -64,28 +96,35 @@ class App extends React.Component {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="collapsibleNavbar">
-            <form className="form-inline form-row ml-auto">
-              <input className="form-control mt-2 mb-2 mx-2 mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-              <button className="btn btn-outline-light mt-2 mb-2 mx-2" type="submit">Search Tag</button>
+            <form ref="search" className="form-inline form-row ml-auto">
+              <input ref="wordsearch" className="form-control mt-2 mb-2 mx-2 mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+              <button className="btn btn-outline-light mt-2 mb-2 mx-2" onClick={(e) => this.fSearch(e)}>Search Tag</button>
             </form>
           </div>
         </nav>
         <div className="App container-fluid">
           <form ref="myForm">
-            <div className="form group">
-              <label id="addNotes">Add Notes:</label>
+            <div className="form-group-lg">
+              <label id="addNotes" className="mt-2 mb-2 mx-2">Add Notes:</label>
               <input type="text" ref="note" placeholder="Your new note..." className="formField" />
-              <button type="submit" className="btn btn-outline-light mt-2 mb-2" style={{ background: "rgba(52, 20, 249, 0.57)" }} onClick={(e) => this.fSubmit(e)}>Add Note</button>
+              <label id="addTags" className="mt-2 mb-2 mx-2">Add Tags:</label>
+              <input type="text" ref="tags" placeholder="e.g. tag1,tag2,tag3" className="formField" />
+              <button type="submit" className="btn btn-outline-light  mt-2 mb-2 mx-2" style={{ background: "rgba(52, 20, 249, 0.57)" }} onClick={(e) => this.fSubmit(e)}>Add Note</button>
             </div>
           </form>
           <ul className="list-group">
             {datas.map((data, i) =>
-              <li key={i} className="list-group-item d-flex flex-wrap">
+              <li key={i} className="list-group-item d-flex flex-wrap justify-content-between">
                 <div>
-                  <div className="d-flex flex-wrap">Note: {data.note}</div>
+                  <p className="text-justify text-break">Note: {data.note}</p>
                   <div className="d-flex flex-wrap">Date: {data.date}</div>
+                  <div className="d-flex flex-wrap">
+                    {data.tags.map((tag) =>
+                      <span className="badge badge-primary mt-2 mb-2 mx-2" style={{ background: "rgba(52, 20, 249, 0.57)" }}>{tag}</span>
+                    )}
+                  </div>
                 </div>
-                <div className="ml-auto">
+                <div>
                   <button onClick={() => this.fRemove(i)} className="btn btn-outline-dark mt-2 mb-2 mx-2">remove </button>
                 </div>
               </li>
