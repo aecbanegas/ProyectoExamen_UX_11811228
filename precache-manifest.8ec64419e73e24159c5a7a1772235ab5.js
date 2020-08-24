8fbcc396006629d51e4fc57c23f3439b97eeca5d
Andrés Cruz <andrescruz@Andress-MacBook-Pro.local>
2020-08-23 21:56:05 -0600

    Updates

---
+++ precache-manifest.8ec64419e73e24159c5a7a1772235ab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497e1b436f4274f8c501379685052c0c",
+    "url": "/ProyectoExamen_UX_11811228/index.html"
+  },
+  {
+    "revision": "1cf10a091620f8766832",
+    "url": "/ProyectoExamen_UX_11811228/static/css/main.361da6de.chunk.css"
+  },
+  {
+    "revision": "f813b5ed9d879e68bb3e",
+    "url": "/ProyectoExamen_UX_11811228/static/js/2.7ba041ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ProyectoExamen_UX_11811228/static/js/2.7ba041ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf10a091620f8766832",
+    "url": "/ProyectoExamen_UX_11811228/static/js/main.89dd80c2.chunk.js"
+  },
+  {
+    "revision": "833e5ac1898b4fa044c7",
+    "url": "/ProyectoExamen_UX_11811228/static/js/runtime-main.07556e44.js"
+  }
+]);
